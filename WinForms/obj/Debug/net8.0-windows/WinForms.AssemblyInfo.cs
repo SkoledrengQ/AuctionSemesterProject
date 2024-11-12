6c86c570fafc9ade676f936d31a10ecadfba6ed5
SkoledrengQ <116273731+SkoledrengQ@users.noreply.github.com>
2024-11-12 17:09:46 +0100

    Updates WindowsForms
    Updated Winforms & removed the old Winforms for compatibility

--- WinForms/obj/Debug/net8.0-windows/WinForms.AssemblyInfo.cs
+++ WinForms/obj/Debug/net8.0-windows/WinForms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+727044e529c8db7ca533c09eae61976bf35b0fc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
