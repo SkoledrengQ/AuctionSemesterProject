f62216efe2bf61ab756114f852972b4ba5e5c439
Morten812 <150682994+Morten812@users.noreply.github.com>
2024-11-28 19:47:54 +0100

    asd

--- AuctionWebApp/obj/Debug/net8.0/AuctionWebApp.AssemblyInfo.cs
+++ AuctionWebApp/obj/Debug/net8.0/AuctionWebApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuctionWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8330d56d545c2d2940578ef036fd1ad0189ebcc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e767b6ce3fbd8743a37f808467f27109d825f4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuctionWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuctionWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
