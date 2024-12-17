60c4ae8b51853639410715b57e39802d9b9c3331
Morten812 <150682994+Morten812@users.noreply.github.com>
2024-12-17 22:50:23 +0100

    asdasd

--- WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
+++ WinFormsApp1/obj/Debug/net8.0-windows/WinFormsApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b189daa727e764a8c7a046401c555f47a73682d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aefb171f4e3f239efd076c64c1c29251276ea112")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
